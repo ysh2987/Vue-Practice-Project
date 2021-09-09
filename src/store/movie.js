7c179076f6874ae0703a610f3a32fa1722a05c63
ysh2987 <ysh2987@naver.com>
2021-09-09 20:45:54 +0900

    Start project

---
+++ src/store/movie.js
@@ -0,0 +1,95 @@
+import axios from 'axios'
+import _uniqBy from 'lodash/uniqBy'
+
+export default{
+    namespaced: true,
+    state: function()  {
+        return{
+            movies: [],
+            message: 'Search for the movie title!',
+            loading: false
+        }
+    },
+    getters: {},
+    mutations: {
+        updateState(state, payload){
+            Object.keys(payload).forEach(key => {
+                state[key] = payload[key]
+            })
+        },
+        resetMovies(state){
+            state.movies = []
+        }
+    },
+    actions: {
+        async searchMovies({ state, commit }, payload){
+            if(state.loading) return
+            commit('updateState', {
+                message: '',
+                loading: true
+            })
+
+            try{
+                const res = await _fetchMoive({
+                    ...payload,
+                    page: 1
+                })
+                const { Search, totalResults } = res.data
+                commit('updateState',{
+                    movies: _uniqBy(Search, 'imdbID')
+                })
+                console.log(totalResults)
+                console.log(typeof totalResults)
+    
+                const total = parseInt(totalResults, 10)
+                const pageLength = Math.ceil(total / 10)
+    
+                if(pageLength > 1){
+                    for(let page = 2; page <= pageLength; page++){
+                        if (page > (payload.number / 10))break
+                        const res = await _fetchMoive({
+                            ...payload,
+                            page
+                        })
+                        const { Search } = res.data
+                        commit('updateState', {
+                            movies: [
+                                ...state.movies,
+                                ..._uniqBy(Search, 'imdbID')
+                            ]
+                        })
+                    }
+                }
+            } catch(message){
+                commit('updateState', {
+                    movies: [],
+                    message
+                })
+            } finally {
+                commit('updateState', {
+                    loading: false
+                })
+            }
+           
+                
+        } 
+    }
+}
+
+function _fetchMoive(payload){
+    const {title, type, year, page} = payload
+    const OMDB_API_KEY = '7035c60c'
+    const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+    return new Promise( (resolve, reject) =>{
+        axios.get(url)
+            .then(res=>{
+                if(res.data.Error){
+                    reject(res.data.Error)
+                }
+                resolve(res)
+            })
+            .catch((err)=> {
+                reject(err.message)
+            })
+    })
+}
