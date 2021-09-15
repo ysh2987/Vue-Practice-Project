21870d53f86d9c0ba802188b73c135edd4bee0e1
ysh2987 <ysh2987@naver.com>
2021-09-15 23:44:52 +0900

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,28 @@
-import { createRouter, createWebHashHistory} from 'vue-router'
-import Home from'./Home'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
 import Movie from './Movie'
-import About from './about'
+import About from './About'
 import NotFound from './NotFound'
-export default createRouter ({
-  // Hash
-  // http://google.com/#/search
+
+export default createRouter({
   history: createWebHashHistory(),
-  scrollBehavior(){
+  scrollBehavior() {
     return { top: 0 }
   },
-  // pages
   routes: [
     {
       path: '/',
       component: Home
     },
-    { 
+    {
       path: '/movie/:id',
       component: Movie
     },
-    { // 페이지경로와 해당 페이지에 연결할 컴포넌트 작성
+    {
       path: '/about',
       component: About
     },
-    { 
+    {
       path: '/:notFound(.*)',
       component: NotFound
     }
