aab941176fe5212ec5760cc8ce32bf75d6aab355
ysh2987 <ysh2987@naver.com>
2021-09-16 01:33:00 +0900

    netlify

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader') 
-
+const Dotenv = require('dotenv-webpack')
 module.exports = {
   resolve: {
     extensions : ['.js', '.vue'],
@@ -70,13 +70,14 @@ module.exports = {
         { from: 'static' }
       ]
     }),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    new Dotenv()
   ],
 
   // 개발 서버 옵션
   devServer: {
     host: 'localhost',
-    port: 8080,
+    port: 8079,
     hot: true
   }
 }
